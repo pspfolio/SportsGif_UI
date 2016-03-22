5505c2c55a262a5e63332645fd2eb09ceba3ffe7
PeppeS <pspfolio@gmail.com>
2016-03-22 18:14:35 +0200

    Getting data from service in directive

--- src/components/gifs/gifs-directive.js
+++ src/components/gifs/gifs-directive.js
@@ -1,11 +1,30 @@
 (function() {
 
+    function gifsCtrl($scope, GifFactory) {
+        var vm = this;
+        var category = $scope.category;
+        vm.gifs = GifFactory.gifs;
+        vm.selectedGif = {};
+
+        vm.setGif = function(gif) {
+            vm.selectedGif = gif;
+        };
+        console.log(category);
+
+        GifFactory.getGifs(category).then(function() {
+            vm.gifs = GifFactory.gifs;
+        });
+    };
+
     function gifs() {
         return {
             restrict: 'E',
             scope: {
-                category: '@'
+                category: '@',
+                limit: '@'
             },
+            controller: gifsCtrl,
+            controllerAs: 'vm',
             templateUrl: 'src/components/gifs/gifs.html'
         }
     }
