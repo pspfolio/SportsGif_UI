37768ef24f9ee4514961d0d0240e85d6a177d62d
PeppeS <pspfolio@gmail.com>
2016-05-07 09:49:44 +0300

    Adding loadmore button more userfriendly than infintescrolling

--- src/components/gifs/gifs-directive.js
+++ src/components/gifs/gifs-directive.js
@@ -2,16 +2,36 @@
     
 	function gifsCtrl(GifFactory, $routeParams) {
 		var vm = this;
+		var page = 0;
 		
 		var category = vm.category || $routeParams.subCategory;
 		
 		GifFactory.getGifs(category, vm.limit).then(function () {
-			vm.gifs = GifFactory.gifs;
+			vm.gifs = sliceGifs(0);
 		});
 		
 		vm.selectGif = function(gif) {
 			vm.selgif = gif;
 		};
+		
+		vm.loadMore = function() {
+			page++;
+			var newGifs = sliceGifs(page*50);
+			console.log(newGifs);
+			vm.gifs = vm.gifs.concat(newGifs);
+		};
+		
+		// move to factory / get data always from backend
+		function sliceGifs(skip) {
+			var gifs = null;
+			if(skip === 0) {
+				gifs = GifFactory.gifs.slice(0, 50);
+			} else {
+				gifs = GifFactory.gifs.slice(skip, skip*2);
+			}
+			
+			return gifs;
+		}
 	}
 
 	function gifs() {
