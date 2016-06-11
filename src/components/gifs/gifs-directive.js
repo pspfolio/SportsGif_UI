f83a69d7effa00f247c2541c24e3281c73bd4e6d
PeppeS <pspfolio@gmail.com>
2016-06-11 09:56:47 +0300

    Moving data to upper ctrl from directive

--- src/components/gifs/gifs-directive.js
+++ src/components/gifs/gifs-directive.js
@@ -2,47 +2,20 @@
     
 	function gifsCtrl($scope, GifFactory, FilterFactory, $routeParams) {
 		var vm = this;
-		var page = 0;
-		var category = vm.category || $routeParams.subCategory;
-		var filter = FilterFactory.getFilter();
-		FilterFactory.filterChangedSubscribe($scope, handleFilterChanged);
 
 		vm.selectGif = function(gif) {
-			GifFactory.updateView(gif._id);
 			vm.gifSelected({gif: gif});
 		};
-		
-		vm.loadMore = function() {
-			page++;
-			getGifs();
-		};
-		
-		getGifs();
-		
-		function getGifs() {
-			GifFactory.getGifs(vm.category, vm.limit, page, filter).then(handleGifs);
-		}
-		
-		function handleFilterChanged() {
-			page = 0;
-			vm.gifs = [];
-			filter = FilterFactory.getFilter();
-			getGifs();
-		}
-		
-		function handleGifs(gifs) {
-			vm.showButton = gifs.data.length === vm.limit;
-			vm.gifs =  vm.gifs ? vm.gifs.concat(gifs.data) : gifs.data;
-		}
 	}
 
 	function gifs() {
 		return {
 			restrict: 'E',
 			scope: {
-				category: '=',
-				limit: '=',
-				gifSelected: '&'
+				gifs: '=',
+				showButton: '=',
+				gifSelected: '&',
+				gifLoadMore: '&'
 			},
 			controller: gifsCtrl,
 			controllerAs: 'vm',
--- src/partials/mainpage.controller.js
+++ src/partials/mainpage.controller.js
@@ -3,14 +3,45 @@
 	
 	angular
 		.module('app')
-		.controller('mainpageCtrl', frontpageCtrl);
+		.controller('mainpageCtrl', mainpageCtrl);
 	
-	function frontpageCtrl($rootScope) {
+	function mainpageCtrl($rootScope, $scope, GifFactory, FilterFactory) {
+		var category = "nba";
+		var limit = 10;
 		var vm = this;
-		vm.category = "nba";
+		var page = 0;
+				
+		// GIFS
+		
+		getGifs();
+		
+		function getGifs() {
+			GifFactory.getGifs(category, limit, page, filter).then(handleGifs);
+		}
+		
+		function handleGifs(gifs) {
+			vm.showButton = gifs.data.length === limit;
+			vm.gifs =  vm.gifs ? vm.gifs.concat(gifs.data) : gifs.data;
+		}
+		
 		vm.gifSelected = function(gif) {
+			GifFactory.updateView(gif._id);
 			$rootScope.$broadcast('playGif', gif);
 		}
-
+		
+		vm.gifLoadMore = function() {
+			page++;
+			getGifs();
+		}
+		
+		// FILTERS
+		FilterFactory.filterChangedSubscribe($scope, handleFilterChanged);
+		var filter = FilterFactory.getFilter();
+		function handleFilterChanged() {
+			page = 0;
+			vm.gifs = [];
+			filter = FilterFactory.getFilter();
+			getGifs();
+		}
 	}
 }());
