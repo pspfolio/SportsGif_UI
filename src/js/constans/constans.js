4ea770f4d0731e63bc37be22edf81073c00a8bc8
PeppeS <pspfolio@gmail.com>
2016-06-12 21:30:41 +0300

    updating api_gifs url

--- src/js/constans/constans.js
+++ src/js/constans/constans.js
@@ -3,6 +3,6 @@
 	
 	angular
 		.module('app')
-		.constant('API_GIFS_URL', 'http://localhost:1337/api/gifs/')
-		.constant('API_GIFS_UPDATE_VIEW', 'http://localhost:1337/api/views/')
+		.constant('API_GIFS_URL', 'http://139.59.136.224:3000/api/gifs/')
+		.constant('API_GIFS_UPDATE_VIEW', 'http://139.59.136.224:3000/api/views/')
 }());
